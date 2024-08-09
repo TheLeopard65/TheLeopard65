e2173831ad0464b7c2fdbd6caa495374518a14f2
Yasir Mehmood <152008995+TheLeopard65@users.noreply.github.com>
2024-08-09 19:47:27 +0500

    Update Fetch-my-rank.py

--- Fetch-my-rank.py
+++ Fetch-my-rank.py
@@ -3,10 +3,16 @@ from PIL import Image
 import time
 
 def HTB_screenshot(url, x, y, width, height, output_file):
+    if os.path.exists(screenshot_file):
+        os.remove(screenshot_file)
+    if os.path.exists(output_file):
+        os.remove(output_file)
+    
     driver = webdriver.Chrome()
     driver.get(url)
     time.sleep(5)
     driver.save_screenshot('profile.png')
+    
     with Image.open('profile.png') as image:
         cropped_img = image.crop((x, y, x + width, y + height))
         cropped_img.save(output_file)
